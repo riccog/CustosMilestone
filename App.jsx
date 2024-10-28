701ab3a617e17fbad27a13a6770f4ad14e6a47fe
uhohitsmel <149434384+uhohitsmel@users.noreply.github.com>
2024-10-27 21:12:17 -0400

    added the redirect uri endpoint

--- App.jsx
+++ App.jsx
@@ -11,14 +11,18 @@ function App() {
   const Logon = () => {
     const tenantID = 10000000;
     const baseURL = `https://api.playground.usecustos.org/api/v1/identity-management/tenant/${tenantID}/.well-known/openid-configuration`;
+    const tenantID = 10000000;
+    const baseURL = `https://api.playground.usecustos.org/api/v1/identity-management/tenant/${tenantID}/.well-known/openid-configuration`;
     axios
-        .get(baseURL)
-        .then(res => {
-          console.log(res.data.authorization_endpoint)
-        })
-        .catch(error => {
-          console.error(error);
-        })
+      .get(baseURL)
+      .then(res => {
+        const auth = res.data.authorization_endpoint;
+        // Redirect the user to the authorization endpoint
+        window.location.href = `${auth}?client_id=custos-qt5ye0xuq9fiszscvhpn-10000000&redirect_uri=http://localhost:5173/auth/callback&scope=openid email&state=active&response_type=code`;
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   return (
